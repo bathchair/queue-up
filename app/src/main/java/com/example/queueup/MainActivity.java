aedc83cf76248917585f49fd81ebfa739f9fa03d
bathchair <dea@udel.edu>
2022-12-06 20:07:03 -0500

    code: profile screen functioning

--- app/src/main/java/com/example/queueup/MainActivity.java
+++ app/src/main/java/com/example/queueup/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.queueup;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    private View mainScreen;
+    private View profileScreen;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final Context context = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainScreen = findViewById(R.id.mainScreen);
+        profileScreen = findViewById(R.id.profileScreen);
+
+        ImageButton profileButton = findViewById(R.id.profileButton);
+        profileButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(context, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
