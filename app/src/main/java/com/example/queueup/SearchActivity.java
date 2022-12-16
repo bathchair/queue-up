eff7c3af2bf7d6d7a8aa55bf0fe593ce2ad2631c
Ren Ross <briross@udel.edu>
2022-12-16 05:38:39 -0500

    completed Search Task

--- app/src/main/java/com/example/queueup/SearchActivity.java
+++ app/src/main/java/com/example/queueup/SearchActivity.java
@@ -3,9 +3,11 @@ import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.SeekBar;
 import android.widget.ArrayAdapter;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,60 +22,83 @@ public class SearchActivity extends AppCompatActivity {
 
         // Circular Search Button
         ImageButton searchBtn = findViewById(R.id.search_btn);
-        searchBtn.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View V) {
-                Intent intent = new Intent(context, SearchResultActivity.class);
-                startActivity(intent);
-            }
+        searchBtn.setOnClickListener(V -> {
+            Intent intent = new Intent(context, SearchResultActivity.class);
+            startActivity(intent);
         });
 
         // Filter Buttons
-        ImageButton selectGame = findViewById(R.id.select_game_btn);
         Spinner gameSpinner = findViewById(R.id.game_spinner);
         ArrayAdapter<CharSequence> gameAdapter = ArrayAdapter.createFromResource(this,
-                R.array.game_dropdown, android.R.layout.simple_spinner_item);
-        selectGame.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View V) { // TODO
-                gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-                gameSpinner.setAdapter(gameAdapter);
+                R.array.game_dropdown, R.layout.spinner);
+        gameSpinner.setSelection(0);
+        gameSpinner.setAdapter(gameAdapter);
+        gameSpinner.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent e) {
+                if (e.getAction() == MotionEvent.ACTION_UP) {
+                    gameAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                    gameSpinner.setAdapter(gameAdapter);
+                }
+                return false;
             }
         });
 
-        ImageButton selectRank = findViewById(R.id.select_rank_btn);
         Spinner rankSpinner = findViewById(R.id.rank_spinner);
         ArrayAdapter<CharSequence> rankAdapter = ArrayAdapter.createFromResource(this,
-                R.array.rank_dropdown, android.R.layout.simple_spinner_item);
-        selectRank.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View V) { // TODO
-                rankAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-                rankSpinner.setAdapter(rankAdapter);
+                R.array.rank_dropdown, R.layout.spinner);
+        rankSpinner.setSelection(0);
+        rankSpinner.setAdapter(rankAdapter);
+        rankSpinner.setOnTouchListener(new View.OnTouchListener() {
+            public boolean onTouch(View v, MotionEvent e) {
+                if (e.getAction() == MotionEvent.ACTION_UP) {
+                    rankAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+                    rankSpinner.setAdapter(rankAdapter);
+                }
+                return false;
             }
+        });
+
+        // Location & Age SeekBars
+        SeekBar locationSeek = (SeekBar) findViewById(R.id.locationSeekBar);
+        locationSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int currRange = locationSeek.getProgress();
+            public void onProgressChanged(SeekBar s, int p, boolean fromUser) {
+                currRange = p;
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+        });
+
+        SeekBar ageSeek = (SeekBar) findViewById(R.id.ageSeekBar);
+        ageSeek.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            int currRange = ageSeek.getProgress();
+            public void onProgressChanged(SeekBar s, int p, boolean fromUser) {
+                currRange = p;
+            }
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            public void onStopTrackingTouch(SeekBar seekBar) {}
         });
 
         // Navigation Bar Buttons
         ImageButton homeButton = findViewById(R.id.homeButton);
-        homeButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(context, MainActivity.class);
-                startActivity(intent);
-            }
+        homeButton.setOnClickListener(v -> {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
         });
 
         ImageButton resultButton = findViewById(R.id.notificationButton);
-        resultButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // TODO
-            }
+        resultButton.setOnClickListener(v -> {
+            // TODO: notification button
         });
 
         // TODO: chat button
 
         ImageButton profileButton = findViewById(R.id.profileButton);
-        profileButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                Intent intent = new Intent(context, ProfileActivity.class);
-                startActivity(intent);
-            }
+        profileButton.setOnClickListener(v -> {
+            Intent intent = new Intent(context, ProfileActivity.class);
+            startActivity(intent);
         });
     }
 }
